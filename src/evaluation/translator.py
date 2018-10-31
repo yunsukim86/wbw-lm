0d2e7216fb985d69f91f5fa75ee8f490aa883c4f
Yunsu Kim <kim@cs.rwth-aachen.de>
2018-10-31 23:01:44 +0100

    initial version of wbw-lm

---
+++ src/evaluation/translator.py
@@ -0,0 +1,155 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# Copyright (c) 2018-present, Yunsu Kim
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+
+from logging import getLogger
+from math import exp, log
+import os
+
+import numpy as np
+import torch
+
+from ..utils import get_nn_avg_dist
+
+logger = getLogger('src/evaluation/translator.py')
+
+
+# constants
+UNK_TOKEN = '<unk>'
+EOS_TOKEN = '</s>'
+CATEGORY_LABELS = ['$number', '$url']
+PROB_DEFAULT = 0.01
+
+
+# scaling functions for similarity scores
+def linear_scaling(x):
+    return ((x + 1) / 2).tolist()
+
+def sigmoid(x):
+    return np.exp(x) / (1 + np.exp(x))
+
+def softmax(x, T=1):
+    x = x / T  # temparature
+    e_x = np.exp(x - np.max(x))  # max() for numerical stability
+    return e_x / e_x.sum()
+
+
+class Translator(object):
+
+    def __init__(self, src_emb, tgt_emb, src_dico, tgt_dico, params):
+        self.emb1 = src_emb.weight.data
+        self.emb2 = tgt_emb.weight.data
+        self.src_dico = src_dico
+        self.tgt_dico = tgt_dico
+        self.params = params
+
+        # normalize embeddings
+        self.emb1 = self.emb1 / self.emb1.norm(2, 1, keepdim=True).expand_as(self.emb1)
+        self.emb2 = self.emb2 / self.emb2.norm(2, 1, keepdim=True).expand_as(self.emb2)
+        
+        # precompute hub penalty terms for csls
+        if self.params.similarity_measure == 'csls':
+            self.average_dist1 = get_nn_avg_dist(self.emb2, self.emb1, self.params.csls_k)
+            self.average_dist2 = get_nn_avg_dist(self.emb1, self.emb2, self.params.csls_k)
+            self.average_dist1 = torch.from_numpy(self.average_dist1).type_as(self.emb1)
+            self.average_dist2 = torch.from_numpy(self.average_dist2).type_as(self.emb2)
+
+    # src_word -> [topk_tgt_words], [topk_scores]
+    def word_translation(self, src_word):
+        if src_word not in self.src_dico.word2id:  # source unknown word
+            if self.params.unknown_words == 'copy':
+                return [src_word], [PROB_DEFAULT]
+            elif self.params.unknown_words == 'unk':
+                return ['<unk>'], [PROB_DEFAULT]
+            else:
+                raise ValueError('Wrong argument for --unknown_words')
+        elif src_word in CATEGORY_LABELS:
+            return [src_word], [PROB_DEFAULT]
+        else:
+            src_word_id = self.src_dico.word2id[src_word]
+            src_word_vec = self.emb1[src_word_id].view(1, self.params.emb_dim)
+
+            # nearest neighbor
+            scores = src_word_vec.mm(self.emb2.transpose(0,1))
+            if self.params.similarity_measure == 'csls':
+                scores.mul_(2)
+                scores.sub_(self.average_dist1[[src_word_id]][:, None])
+                scores.sub_(self.average_dist2[None, :])
+
+            topk = scores.topk(self.params.topk, 1, True)
+            topk_scores = topk[0].cpu().numpy()[0]
+            topk_tgt_ids = topk[1]
+
+            # scaling of similarity scores
+            if self.params.emb_scaling == 'linear':
+                topk_scores = linear_scaling(topk_scores)
+            elif self.params.emb_scaling == 'sigmoid':
+                topk_scores = sigmoid(topk_scores)
+            elif self.params.emb_scaling == 'softmax':
+                topk_scores = softmax(topk_scores, self.params.softmax_temparature)
+
+            # convert from ids to words
+            topk_tgt_words = []
+            for id_list in topk_tgt_ids:
+                for i in id_list:
+                    tgt_word = self.tgt_dico.id2word[i]
+                    if tgt_word not in CATEGORY_LABELS and tgt_word != EOS_TOKEN:
+                        topk_tgt_words.append(tgt_word)
+
+            if len(topk_tgt_words) == 0:
+                topk_tgt_words = [src_word]
+                topk_scores = [PROB_DEFAULT]
+
+            print(src_word)
+            print(topk_scores)
+
+            return topk_tgt_words, topk_scores
+
+    def sent_translation(self, sent, lm):
+        beam = [[list(), 0.0]]
+        for n in range(len(sent)):
+            src_word = sent[n]
+            if type(src_word) == bytes:
+                src_word = src_word.decode()
+            topk_tgt_words, topk_scores = self.word_translation(src_word)
+            
+            if lm:
+                all_candidates = list()
+
+                topk_zipped = list(zip(topk_tgt_words, topk_scores))
+                for sequence, sequence_score in beam:
+                    lm_score_history = lm.score(' '.join(sequence), bos=True, eos=False)
+                    for tgt_word, score in topk_zipped:
+                        eos = False
+                        if n == len(sent) - 1:
+                            eos = True
+                        new_sequence = sequence + [tgt_word]
+                        lex_score = self.params.lex_scaling * log(score)
+                        lm_score = self.params.lm_scaling * (lm.score(' '.join(new_sequence), bos=True, eos=eos) - lm_score_history)
+                        new_sequence_score = sequence_score + lex_score + lm_score
+                        all_candidates.append([new_sequence, new_sequence_score])
+                ordered = sorted(all_candidates, key=lambda x: x[1], reverse=True)
+                beam = ordered[:self.params.beam_size]
+            else:
+                beam[0][0].append(topk_tgt_words[0])
+                beam[0][1] += log(topk_scores[0])
+        return beam[0][0]
+
+    def corpus_translation(self, corpus, lm):
+        translations = []
+        for sent in corpus:
+            trs = self.sent_translation(sent, lm)
+            translations.append(trs)
+
+        output_path = os.path.join(self.params.output)
+        with open(output_path, 'w') as output_file:
+            for trs in translations:
+                output_file.write("%s\n" % ' '.join(trs))
+        logger.info('Writing translations to %s ...' % output_path)
---
+++ translate.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# Copyright (c) 2018-present, Yunsu Kim
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+
+import argparse
+import logging
+import os
+import sys
+
+import kenlm
+from torch import nn
+
+from src.utils import bool_flag, load_embeddings, normalize_embeddings
+from src.evaluation.translator import Translator
+
+
+def is_zip(fname):
+    fp = open(fname, "rb")
+    magic = fp.read(2)
+    fp.close()
+    return magic == '\x1f\x8b'
+
+def open_file(file_arg, allowNone=True):
+    if not file_arg:
+        if allowNone:
+            return None
+        else:
+            raise IOError
+    if isinstance(file_arg, str):
+        if is_zip(file_arg):
+            return gzip.GzipFile(file_arg)
+        else:
+            return open(file_arg)
+    elif isinstance(file_arg, file):
+        return file_arg
+
+def load_input(filename, lowercase):
+    input_stream = sys.stdin
+    if filename is not None:
+        input_stream = open_file(filename)
+    input_sents = []
+    for line in input_stream:
+        if lowercase:
+            line = line.lower()
+        input_sents.append(line.rstrip().split())
+    return input_sents
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Evaluation')
+
+    # data
+    parser.add_argument('--input', type=str, help='Source corpus to translate (default: stdin)')
+    parser.add_argument('--output', type=str, help='File name where the output translations will be written (default: stdout)')
+    parser.add_argument('--input_lowercase', type=bool_flag, default=True, help='Lowercase the given input corpus')
+
+    # embedding
+    parser.add_argument('--src_emb', type=str, required=True, help='Source embeddings')
+    parser.add_argument('--tgt_emb', type=str, required=True, help='Target embeddings')
+    parser.add_argument('--emb_dim', type=int, required=True, help='Embedding dimension')
+    parser.add_argument('--normalize_embeddings', type=str, default='', help='Normalize embeddings before translation')
+
+    # search
+    parser.add_argument('--max_vocab', type=int, default=100000, help='Maximum source/target vocabulary size considered in search')
+    parser.add_argument('--similarity_measure', type=str, default='csls', help='Similarity measure used for nearest neighbor search (cosine|csls)')
+    parser.add_argument('--csls_k', type=int, default=10, help='Number of nearest neighbors for computing hub penalty term in csls')
+    parser.add_argument('--unknown_words', type=str, default='copy', help='Method to deal with unknown source word (copy|unk)')
+
+    # lm integration
+    parser.add_argument('--lm', type=str, help='Target language model file')
+    parser.add_argument('--lm_scaling', type=float, default=0.1, help='Language model scaling')
+    parser.add_argument('--lex_scaling', type=float, default=1.0, help='Lexicon model scaling')
+    parser.add_argument('--beam_size', type=int, default=10, help='Beam size')
+    parser.add_argument('--topk', type=int, default=100, help='Number of nearest neighbors considered')
+    parser.add_argument('--emb_scaling', type=str, default='linear', help='(linear|sigmoid|softmax)')
+    parser.add_argument('--softmax_temparature', type=float, default=1.0, help='Temparature parameter for scaling softmax distribution')
+
+    # others
+    parser.add_argument('--cuda', type=bool_flag, default=True, help='Run on GPU')
+
+    # parse parameters
+    params = parser.parse_args()
+    params.src_lang = 'src'
+    params.tgt_lang = 'tgt'  # lang code can be arbitrary since we don't load dictionary files
+
+    # check parameters
+    assert os.path.isfile(params.src_emb)
+    assert os.path.isfile(params.tgt_emb)
+
+    # logger
+    logger = logging.getLogger('translate.py')  # CHECK: random seeding necessary?
+
+    # load input (to translate)
+    logger.info("loading input data...")
+    input_sents = load_input(params.input, params.input_lowercase)  # CHECK: vocab?
+
+    # load embeddings
+    logger.info("loading embeddings...")
+    src_dico, _src_emb = load_embeddings(params, source=True)  # 'dico' = word2id mappings
+    src_emb = nn.Embedding(len(src_dico), params.emb_dim, sparse=True)
+    src_emb.weight.data.copy_(_src_emb)
+    
+    tgt_dico, _tgt_emb = load_embeddings(params, source=False)
+    tgt_emb = nn.Embedding(len(tgt_dico), params.emb_dim, sparse=True)
+    tgt_emb.weight.data.copy_(_tgt_emb)
+
+    if params.cuda:
+        src_emb.cuda()
+        tgt_emb.cuda()
+
+    # normalize embeddings
+    logger.info("normalizing embeddings...")
+    params.src_mean = normalize_embeddings(src_emb.weight.data, params.normalize_embeddings)
+    params.tgt_mean = normalize_embeddings(tgt_emb.weight.data, params.normalize_embeddings)
+    
+    # load lm
+    lm = None
+    if params.lm is not None:
+        logger.info("loading LM...")
+        lm = kenlm.LanguageModel(params.lm)
+
+    # translate
+    logger.info("translating...")
+    translator = Translator(src_emb, tgt_emb, src_dico, tgt_dico, params)
+    translator.corpus_translation(input_sents, lm)
